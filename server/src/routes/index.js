dc2fa6fa0d42c3bd24e7800639839a03de22ffa5
dropbeardan <dan.ess.001@gmail.com>
2018-01-18 11:41:57 +1100

    Changed Image repository to a Resource repository to broaden use case.

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,16 +1,16 @@
 const routes = require('express').Router();
 
 const frontEnd = require('./frontEnd');
-const images = require('./images');
 const journeys = require('./journeys');
+const resources = require('./resources');
 const services = require('./services');
 const sessions = require('./sessions');
 const stats = require('./stats');
 
 // Normal Routes.
 routes.use('/api/:serviceName', services);
-routes.use('/images', images);
 routes.use('/journeys', journeys);
+routes.use('/resources', resources);
 routes.use('/sessions', sessions);
 routes.use('/stats', stats);
 
